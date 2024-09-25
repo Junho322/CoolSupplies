db7d75b27e2005e4c1dedd945d3803e1901af812
Shine111111 <shayan.y.d.s@gmail.com>
2024-09-25 11:30:40 -0400

    DEV: Generated Java Files

---
+++ src/ca/mcgill/ecse/coolsupplies/model/Bundle.java
@@ -0,0 +1,146 @@
+/*PLEASE DO NOT EDIT THIS CODE*/
+/*This code was generated using the UMPLE 1.34.0.7242.6b8819789 modeling language!*/
+
+
+import java.util.*;
+
+// line 68 "interation1.ump"
+public class Bundle
+{
+
+  //------------------------
+  // MEMBER VARIABLES
+  //------------------------
+
+  //Bundle Attributes
+  private double discount;
+
+  //Bundle Associations
+  private List<BundleItem> items;
+
+  //------------------------
+  // CONSTRUCTOR
+  //------------------------
+
+  public Bundle()
+  {
+    discount = 0;
+    items = new ArrayList<BundleItem>();
+  }
+
+  //------------------------
+  // INTERFACE
+  //------------------------
+
+  public boolean setDiscount(double aDiscount)
+  {
+    boolean wasSet = false;
+    discount = aDiscount;
+    wasSet = true;
+    return wasSet;
+  }
+
+  public double getDiscount()
+  {
+    return discount;
+  }
+  /* Code from template association_GetMany */
+  public BundleItem getItem(int index)
+  {
+    BundleItem aItem = items.get(index);
+    return aItem;
+  }
+
+  public List<BundleItem> getItems()
+  {
+    List<BundleItem> newItems = Collections.unmodifiableList(items);
+    return newItems;
+  }
+
+  public int numberOfItems()
+  {
+    int number = items.size();
+    return number;
+  }
+
+  public boolean hasItems()
+  {
+    boolean has = items.size() > 0;
+    return has;
+  }
+
+  public int indexOfItem(BundleItem aItem)
+  {
+    int index = items.indexOf(aItem);
+    return index;
+  }
+  /* Code from template association_MinimumNumberOfMethod */
+  public static int minimumNumberOfItems()
+  {
+    return 0;
+  }
+  /* Code from template association_AddUnidirectionalMany */
+  public boolean addItem(BundleItem aItem)
+  {
+    boolean wasAdded = false;
+    if (items.contains(aItem)) { return false; }
+    items.add(aItem);
+    wasAdded = true;
+    return wasAdded;
+  }
+
+  public boolean removeItem(BundleItem aItem)
+  {
+    boolean wasRemoved = false;
+    if (items.contains(aItem))
+    {
+      items.remove(aItem);
+      wasRemoved = true;
+    }
+    return wasRemoved;
+  }
+  /* Code from template association_AddIndexControlFunctions */
+  public boolean addItemAt(BundleItem aItem, int index)
+  {  
+    boolean wasAdded = false;
+    if(addItem(aItem))
+    {
+      if(index < 0 ) { index = 0; }
+      if(index > numberOfItems()) { index = numberOfItems() - 1; }
+      items.remove(aItem);
+      items.add(index, aItem);
+      wasAdded = true;
+    }
+    return wasAdded;
+  }
+
+  public boolean addOrMoveItemAt(BundleItem aItem, int index)
+  {
+    boolean wasAdded = false;
+    if(items.contains(aItem))
+    {
+      if(index < 0 ) { index = 0; }
+      if(index > numberOfItems()) { index = numberOfItems() - 1; }
+      items.remove(aItem);
+      items.add(index, aItem);
+      wasAdded = true;
+    } 
+    else 
+    {
+      wasAdded = addItemAt(aItem, index);
+    }
+    return wasAdded;
+  }
+
+  public void delete()
+  {
+    items.clear();
+  }
+
+
+  public String toString()
+  {
+    return super.toString() + "["+
+            "discount" + ":" + getDiscount()+ "]";
+  }
+}
---
+++ src/ca/mcgill/ecse/coolsupplies/model/Student.java
@@ -0,0 +1,124 @@
+/*PLEASE DO NOT EDIT THIS CODE*/
+/*This code was generated using the UMPLE 1.34.0.7242.6b8819789 modeling language!*/
+
+
+
+// line 56 "interation1.ump"
+public class Student
+{
+
+  //------------------------
+  // STATIC VARIABLES
+  //------------------------
+
+  private static int nextStudentID = 1;
+
+  //------------------------
+  // MEMBER VARIABLES
+  //------------------------
+
+  //Student Attributes
+  private String firstName;
+  private String lastName;
+
+  //Autounique Attributes
+  private int studentID;
+
+  //Student Associations
+  private Grade grade;
+
+  //------------------------
+  // CONSTRUCTOR
+  //------------------------
+
+  public Student(String aFirstName, String aLastName, Grade aGrade)
+  {
+    firstName = aFirstName;
+    lastName = aLastName;
+    studentID = nextStudentID++;
+    boolean didAddGrade = setGrade(aGrade);
+    if (!didAddGrade)
+    {
+      throw new RuntimeException("Unable to create student due to grade. See https://manual.umple.org?RE002ViolationofAssociationMultiplicity.html");
+    }
+  }
+
+  //------------------------
+  // INTERFACE
+  //------------------------
+
+  public boolean setFirstName(String aFirstName)
+  {
+    boolean wasSet = false;
+    firstName = aFirstName;
+    wasSet = true;
+    return wasSet;
+  }
+
+  public boolean setLastName(String aLastName)
+  {
+    boolean wasSet = false;
+    lastName = aLastName;
+    wasSet = true;
+    return wasSet;
+  }
+
+  public String getFirstName()
+  {
+    return firstName;
+  }
+
+  public String getLastName()
+  {
+    return lastName;
+  }
+
+  public int getStudentID()
+  {
+    return studentID;
+  }
+  /* Code from template association_GetOne */
+  public Grade getGrade()
+  {
+    return grade;
+  }
+  /* Code from template association_SetOneToMany */
+  public boolean setGrade(Grade aGrade)
+  {
+    boolean wasSet = false;
+    if (aGrade == null)
+    {
+      return wasSet;
+    }
+
+    Grade existingGrade = grade;
+    grade = aGrade;
+    if (existingGrade != null && !existingGrade.equals(aGrade))
+    {
+      existingGrade.removeStudent(this);
+    }
+    grade.addStudent(this);
+    wasSet = true;
+    return wasSet;
+  }
+
+  public void delete()
+  {
+    Grade placeholderGrade = grade;
+    this.grade = null;
+    if(placeholderGrade != null)
+    {
+      placeholderGrade.removeStudent(this);
+    }
+  }
+
+
+  public String toString()
+  {
+    return super.toString() + "["+
+            "studentID" + ":" + getStudentID()+ "," +
+            "firstName" + ":" + getFirstName()+ "," +
+            "lastName" + ":" + getLastName()+ "]" + System.getProperties().getProperty("line.separator") +
+            "  " + "grade = "+(getGrade()!=null?Integer.toHexString(System.identityHashCode(getGrade())):"null");
+  }
+}
