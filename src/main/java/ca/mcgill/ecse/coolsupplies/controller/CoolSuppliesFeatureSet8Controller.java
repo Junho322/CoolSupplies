bfe3369f187784787e6c82f009fe3156d919b0d9
dyune <david.wang5@mail.mcgill.ca>
2024-10-30 20:56:59 -0400

    Added new feature set for Order SM Iteration 3.

---
+++ src/main/java/ca/mcgill/ecse/coolsupplies/controller/CoolSuppliesFeatureSet8Controller.java
@@ -0,0 +1,27 @@
+package ca.mcgill.ecse.coolsupplies.controller;
+
+public class CoolSuppliesFeatureSet8Controller {
+
+    // TODO: Define your methods first! The methods below are dummy methods, DELETE AND CHANGE THEM.
+
+    public static String addGrade(String level) {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    public static String updateGrade(String level, String newLevel) {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    public static String deleteGrade(String level) {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    public static TOGrade getGrade(String level) {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    // returns all grades
+    public static void getGrades() {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+}
