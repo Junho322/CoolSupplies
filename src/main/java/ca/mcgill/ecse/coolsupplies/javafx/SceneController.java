1dd01f737635ce61e63a6944be1d1649119e72a9
jackmcdonald-byte <jacklmcdonald12@gmail.com>
2024-11-21 17:44:58 -0500

    Implemented SceneController

---
+++ src/main/java/ca/mcgill/ecse/coolsupplies/javafx/SceneController.java
@@ -0,0 +1,57 @@
+package ca.mcgill.ecse.coolsupplies.javafx;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SceneController {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void switchToAdminPage(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("pages/AdminPage.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToBundlePage(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("pages/BundlePage.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToInventoryPage(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("pages/InventoryPage.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToOrdersPage(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("pages/OrdersPage.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToParentPage(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("pages/ParentPage.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
